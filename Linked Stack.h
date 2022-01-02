294147fc45f73e2f19abb75ee7d8e31bdf9f3e36
alperbg <alper0524@gmail.com>
2022-01-02 22:49:46 +0300

    format document

--- Linked Stack.h
+++ Linked Stack.h
@@ -3,104 +3,110 @@ using namespace std;
 
 struct SinglyNode
 {
-	string elem;								// element value
-	SinglyNode* next;						// next item in the list
+    string elem;      // element value
+    SinglyNode *next; // next item in the list
 };
-
 
 struct SinglyLinkedList
 {
-	SinglyNode* head;						// pointer to the head of list
+    SinglyNode *head; // pointer to the head of list
 
-	SinglyLinkedList();						// empty list constructor
-	~SinglyLinkedList();					// destructor
+    SinglyLinkedList();  // empty list constructor
+    ~SinglyLinkedList(); // destructor
 
-	bool empty() const;						// is list empty?
-	const string& front() const;				// get front element
-	void addFront(const string& e);			// add to front of list
-	void removeFront();						// remove front item list
+    bool empty() const;             // is list empty?
+    const string &front() const;    // get front element
+    void addFront(const string &e); // add to front of list
+    void removeFront();             // remove front item list
 };
-
 
 void SinglyLinkedList::removeFront()
-{											// remove front item
-	SinglyNode* old = head;					// save current head
-	head = old->next;						// skip over old head
-	delete old;								// delete the old head
+{                           // remove front item
+    SinglyNode *old = head; // save current head
+    head = old->next;       // skip over old head
+    delete old;             // delete the old head
 }
 
-void SinglyLinkedList::addFront(const string& e)
-{											// add to front of list
-	SinglyNode* v = new SinglyNode;			// create new node
-	v->elem = e;							// store data
-	v->next = head;							// head now follows v
-	head = v;								// v is now the head
+void SinglyLinkedList::addFront(const string &e)
+{                                   // add to front of list
+    SinglyNode *v = new SinglyNode; // create new node
+    v->elem = e;                    // store data
+    v->next = head;                 // head now follows v
+    head = v;                       // v is now the head
 }
 
-SinglyLinkedList::SinglyLinkedList()		// constructor
-{ 
-	head = NULL;
-}
-
-SinglyLinkedList::~SinglyLinkedList()		// destructor
+SinglyLinkedList::SinglyLinkedList() // constructor
 {
-	while (!empty()) removeFront();
+    head = NULL;
 }
 
-bool SinglyLinkedList::empty() const		// is list empty?
+SinglyLinkedList::~SinglyLinkedList() // destructor
 {
-	return head == NULL;
+    while (!empty())
+        removeFront();
 }
 
-const string& SinglyLinkedList::front() const	// get front element
+bool SinglyLinkedList::empty() const // is list empty?
 {
-	return head->elem;
+    return head == NULL;
 }
 
-
-struct LinkedStack						// stack as a linked list
+const string &SinglyLinkedList::front() const // get front element
 {
-	SinglyLinkedList S;					// linked list of elements
-	int n;								// number of elements
+    return head->elem;
+}
 
-	LinkedStack();						// constructor
-	int size() const;					// number of items in the stack
-	bool empty() const;					// is the stack empty?
+struct LinkedStack // stack as a linked list
+{
+    SinglyLinkedList S; // linked list of elements
+    int n;              // number of elements
 
-	const string& top() const ;			// the top element
-	void push(const string& e);  			// push element onto stack
-	void pop() ;						// pop the stack
+    LinkedStack();      // constructor
+    int size() const;   // number of items in the stack
+    bool empty() const; // is the stack empty?
+
+    const string &top() const;  // the top element
+    void push(const string &e); // push element onto stack
+    void pop();                 // pop the stack
 };
 
-LinkedStack::LinkedStack() : S(), n(0)		// constructor
+LinkedStack::LinkedStack() : S(), n(0) // constructor
 {
 }
 
-int LinkedStack::size() const				// number of items in the stack
+int LinkedStack::size() const // number of items in the stack
 {
-	return n;
+    return n;
 }
 
-bool LinkedStack::empty() const				// is the stack empty?
+bool LinkedStack::empty() const // is the stack empty?
 {
-	return n == 0;
+    return n == 0;
 }
 
-const string& LinkedStack::top() const			// return top of stack
+const string &LinkedStack::top() const // return top of stack
 {
-	if (empty()){ cout <<"Top of empty stack !"<<endl; return 0; }
-	return S.front();
+    if (empty())
+    {
+        cout << "Top of empty stack !" << endl;
+        return 0;
+    }
+    return S.front();
 }
 
-void LinkedStack::push(const string& e)		// push element onto the stack
+void LinkedStack::push(const string &e) // push element onto the stack
 {
-	++n;
-	S.addFront(e);
+    ++n;
+    S.addFront(e);
 }
 
-void LinkedStack::pop()						// pop the stack
+void LinkedStack::pop() // pop the stack
 {
-	if (empty()) { cout<<"Pop from empty stack !"<<endl; return; }
-	--n;
-	S.removeFront();
+    if (empty())
+    {
+        cout << "Pop from empty stack !" << endl;
+        return;
+    }
+    --n;
+    S.removeFront();
 }
--- isCurlyBracesMatched.cpp
+++ isCurlyBracesMatched.cpp
@@ -1,47 +1,47 @@
 #include "Linked Stack.h"
-#include <iostream>				// I/O definitions
+#include <iostream> // I/O definitions
 #include <vector>
 #include <fstream>
 #include <string>
 
-using namespace std;				// make std:: accessible
+using namespace std; // make std:: accessible
 
-int countSpaces(string& inputStr);
-void print(std::vector<string>& input);
-string getNumberOfLine(string& inputStr);
+int countSpaces(string &inputStr);
+void print(std::vector<string> &input);
+string getNumberOfLine(string &inputStr);
 
-vector<string> getCurlyBrace() 
+vector<string> getCurlyBrace()
 {
-	vector<string> braces;			// store curly braces lines
+	vector<string> braces; // store curly braces lines
 
 	ifstream fin("a.cpp");
 
 	int lineCount = 0;
 	string line;
-    	string str;
+	string str;
 	int spaces = 0;
 
-	while (getline(fin, line))				// read until end of file 
+	while (getline(fin, line)) // read until end of file
 	{
 		lineCount++;
-        	str = "";
+		str = "";
 
-		int bOpen = line.find("{", 0);			// scan open curly braces
-		int bClose = line.find("}", 0);             	// scan closed curly braces
+		int bOpen = line.find("{", 0);	// scan open curly braces
+		int bClose = line.find("}", 0); // scan closed curly braces
 
-		if (bOpen > -1)					// Found open braces
-		{   
-			spaces = countSpaces(line);       	// get spaces from line
+		if (bOpen > -1) // Found open braces
+		{
+			spaces = countSpaces(line); // get spaces from line
 			for (int i = 0; i < spaces; i++)
 			{
 				str = str + " ";
 			}
-			str = str + "{"+to_string(lineCount);	// add line number to braces vector
+			str = str + "{" + to_string(lineCount); // add line number to braces vector
 			braces.push_back(str);
 
 			str = "";
 			spaces = 0;
-		}// End if
+		}
 
 		if (bClose > -1)
 		{
@@ -50,113 +50,110 @@ vector<string> getCurlyBrace()
 			{
 				str = str + " ";
 			}
-			str = str + "}" + to_string(lineCount); 
+			str = str + "}" + to_string(lineCount);
 
 			braces.push_back(str);
 
 			str = "";
 			spaces = 0;
-		}// End if
-        
-	}// End while
-	return braces;						// return vector of braces
+		}
+
+	}
+	return braces; // return vector of braces
 }
 
-bool isCurlyBracesMatched(const vector<string>& braces) 
-{	
+bool isCurlyBracesMatched(const vector<string> &braces)
+{
 	string wrongCurlyBraces;
-	bool returnValue = true;				//To check the return value	
+	bool returnValue = true; //To check the return value
 	string str;
 	int bOpen;
 	int bClose;
-	LinkedStack S;						// stack for opening braces
-	typedef vector<string>::const_iterator Iter;		// iterator type
+	LinkedStack S;								 // stack for opening braces
+	typedef vector<string>::const_iterator Iter; // iterator type
 	// iterate through vector
-	for (Iter p = braces.begin(); p != braces.end(); ++p) 
-	{ 	
-	
-		bOpen = p->find("{",0);
+	for (Iter p = braces.begin(); p != braces.end(); ++p)
+	{
 
-		if (bOpen > -1)			// is curly braces opened ?
+		bOpen = p->find("{", 0);
+
+		if (bOpen > -1) // is curly braces opened ?
 		{
 			S.push(*p);
 		}
 		else
 		{
-			if(S.empty())   
-			{   				
+			if (S.empty())
+			{
 				wrongCurlyBraces = *p;
-				// Satirda acilmayan bir parantez kapatilmaya calisilmis
-				cout<<"Attempts to close an unopened parenthesis in line "<<getNumberOfLine(wrongCurlyBraces)<<endl; 
+
+				cout << "Attempts to close an unopened parenthesis in line " << getNumberOfLine(wrongCurlyBraces) << endl;
 				returnValue = false;
 			}
 			else
 			{
-				bClose = p->find("}",0);
-				string tempStr = S.top();  		// store the string at the top of the stack
-				int len = countSpaces(tempStr); 	// get spaces length from the top of the stack 
+				bClose = p->find("}", 0);
+				string tempStr = S.top();		// store the string at the top of the stack
+				int len = countSpaces(tempStr); // get spaces length from the top of the stack
 
-				if (( bClose > -1 && len == bClose ))
+				if ((bClose > -1 && len == bClose))
 				{
-					S.pop();			//pop if opened braces are closed
+					//pop if opened braces are closed
+					S.pop();
 				}
-				else if( bClose > -1 && bClose > len)
-				{	
+				else if (bClose > -1 && bClose > len)
+				{
 					wrongCurlyBraces = *p;
-					
-					// Satirda acilmayan bir parantez kapatilmaya calisilmis
-					cout<<"Attempts to close an unopened parenthesis in line "<<getNumberOfLine(wrongCurlyBraces)<<endl; 
-					
-					returnValue = false;  		//sets false the return if spaces do not match
-					
-				}else if (bClose > -1 && bClose < len)
-				{	
+
+					cout << "Attempts to close an unopened parenthesis in line " << getNumberOfLine(wrongCurlyBraces) << endl;
+
+					returnValue = false; //sets false the return if spaces do not match
+				}
+				else if (bClose > -1 && bClose < len)
+				{
 					wrongCurlyBraces = S.top();
-					
-					// Satırda parantez açılmış ama kapatılmamış
-					cout<<"Attempts to open an unclosed parentheses in line "<<getNumberOfLine(wrongCurlyBraces)<<endl;
-					
-					S.pop();   			//error found and used pop for continue the search
-					p--;  				//to check again current p
-					returnValue = false;  		//sets false the return if spaces do not match
-				}	
+
+					cout << "Attempts to open an unclosed parentheses in line " << getNumberOfLine(wrongCurlyBraces) << endl;
+
+					S.pop();			 //error found and used pop for continue the search
+					p--;				 //to check again current p
+					returnValue = false; //sets false the return if spaces do not match
+				}
 			}
 		}
-	} 
-	
-	while (!S.empty())			//if the stack is not empty
-	{	
+	}
+
+	while (!S.empty()) //if the stack is not empty
+	{
 		wrongCurlyBraces = S.top();
-		// Satırda parantez açılmış ama kapatılmamış
-		cout<<"Attempts to open an unclosed parentheses in line "<<getNumberOfLine(wrongCurlyBraces)<<endl;
+		cout << "Attempts to open an unclosed parentheses in line " << getNumberOfLine(wrongCurlyBraces) << endl;
 		S.pop();
-		returnValue = false;		//sets false the return if the stack is not empty
-	}// end while
-	
-	return returnValue;	
-} 
+		returnValue = false; //sets false the return if the stack is not empty
+	}
 
+	return returnValue;
+}
 
-string getNumberOfLine(string& inputStr)  	// Gets number of the braces line for add braces vector
+string getNumberOfLine(string &inputStr) // Gets number of the braces line for add braces vector
 {
 	int len;
 	int bOpen = -1;
 	int bClose = -1;
 	string line = "";
 	len = inputStr.length();
-	bOpen = inputStr.find("{",0);
-	bClose = inputStr.find("}",0);
+	bOpen = inputStr.find("{", 0);
+	bClose = inputStr.find("}", 0);
 
 	if (bOpen > -1)
 	{
-		for (int i = (bOpen + 1) ; i < len; i++)
+		for (int i = (bOpen + 1); i < len; i++)
 		{
 			line = line + inputStr[i];
 		}
-		
-	}else if(bClose > -1)
+	}
+	else if (bClose > -1)
 	{
-		for (int i = (bClose + 1) ; i < len; i++)
+		for (int i = (bClose + 1); i < len; i++)
 		{
 			line = line + inputStr[i];
 		}
@@ -164,13 +161,12 @@ string getNumberOfLine(string& inputStr)  	// Gets number of the braces line for
 	return line;
 }
 
-
-int countSpaces(string& inputStr) 	// counts the spaces of the entered string up to curly braces
+int countSpaces(string &inputStr) // counts the spaces of the entered string up to curly braces
 {
-   	int num = 0;
-   	char space = ' ';
-    	const char TAB = '\t' ;
-	for (int i = 0; i < inputStr.length()  ; i++)
+	int num = 0;
+	char space = ' ';
+	const char TAB = '\t';
+	for (int i = 0; i < inputStr.length(); i++)
 	{
 
 		if (inputStr[i] == '{' || inputStr[i] == '}')
@@ -180,21 +176,26 @@ int countSpaces(string& inputStr) 	// counts the spaces of the entered string up
 		if (inputStr[i] == space)
 		{
 			num++;
-		}else if (inputStr[i] == TAB)
-		{			
+		}
+		else if (inputStr[i] == TAB)
+		{
 			num += 4;
 		}
 	}
-   return 0;
+	return 0;
 }
 
-int main() 
-{ 
-	if(isCurlyBracesMatched(getCurlyBrace())){
-	 	cout<<endl<<"MATCHED"<<endl;
-	 }else
-	 {
-	 	cout<<endl<<"NOT MATCHED"<<endl;
-	 }
+int main()
+{
+	if (isCurlyBracesMatched(getCurlyBrace()))
+	{
+		cout << endl
+			 << "MATCHED" << endl;
+	}
+	else
+	{
+		cout << endl
+			 << "NOT MATCHED" << endl;
+	}
 	return 0;
 }
